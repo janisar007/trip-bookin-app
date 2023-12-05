d6303b9a7dcf6485bca62b6882f9c3737446fcb1
Janisar Akhtar <janisarakhtaransari007@gmail.com>
2023-12-05 19:02:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Offers from './components/Offers'
+import Plan from './components/Plan'
+import Rooms from './components/Rooms'
+import ImageSlider from './components/ImageSlider'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Offers/>
+      <Plan/>
+      <Rooms/>
+      <ImageSlider/>
+    </div>
+  )
+}
+
+export default App
